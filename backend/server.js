64c6d13143c58de9d15abed117e5bc949a7e7a86
Yoselyn Callejas <yoselyncallejas19@gmail.com>
2021-09-13 00:29:23 +0200

    Condition for production/development on deployment

--- backend/server.js
+++ backend/server.js
@@ -23,9 +23,18 @@ app.use("/api/users", userRoutes);
 app.use("/api/appointment", appointmentRoutes);
 app.use("/api/dentists", dentistRoutes);
 
-app.get("/", (req, res) => {
-  res.send("API is running....");
-});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
+
 
 const PORT = process.env.PORT || 5000;
 
